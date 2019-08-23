38e1473bf778e27388ac0681abda0caeece63cbb
Juan Manuel Rodriguez <juarodriguez@grupoassa.com>
2019-08-23 11:18:09 -0300

    webpack and named export

--- public/bundle.js
+++ public/bundle.js
@@ -1,12 +1,85 @@
-// import './utils';
-import {square, add} from './utils';
-import {isAdult, canDrink} from './person';
-console.log('app.js is running');
-console.log('square ', square(4));
-console.log('add', add(2, 3));
+/******/ (function(modules) { // webpackBootstrap
+/******/ 	// The module cache
+/******/ 	var installedModules = {};
+/******/
+/******/ 	// The require function
+/******/ 	function __webpack_require__(moduleId) {
+/******/
+/******/ 		// Check if module is in cache
+/******/ 		if(installedModules[moduleId]) {
+/******/ 			return installedModules[moduleId].exports;
+/******/ 		}
+/******/ 		// Create a new module (and put it into the cache)
+/******/ 		var module = installedModules[moduleId] = {
+/******/ 			i: moduleId,
+/******/ 			l: false,
+/******/ 			exports: {}
+/******/ 		};
+/******/
+/******/ 		// Execute the module function
+/******/ 		modules[moduleId].call(module.exports, module, module.exports, __webpack_require__);
+/******/
+/******/ 		// Flag the module as loaded
+/******/ 		module.l = true;
+/******/
+/******/ 		// Return the exports of the module
+/******/ 		return module.exports;
+/******/ 	}
+/******/
+/******/
+/******/ 	// expose the modules object (__webpack_modules__)
+/******/ 	__webpack_require__.m = modules;
+/******/
+/******/ 	// expose the module cache
+/******/ 	__webpack_require__.c = installedModules;
+/******/
+/******/ 	// define getter function for harmony exports
+/******/ 	__webpack_require__.d = function(exports, name, getter) {
+/******/ 		if(!__webpack_require__.o(exports, name)) {
+/******/ 			Object.defineProperty(exports, name, {
+/******/ 				configurable: false,
+/******/ 				enumerable: true,
+/******/ 				get: getter
+/******/ 			});
+/******/ 		}
+/******/ 	};
+/******/
+/******/ 	// getDefaultExport function for compatibility with non-harmony modules
+/******/ 	__webpack_require__.n = function(module) {
+/******/ 		var getter = module && module.__esModule ?
+/******/ 			function getDefault() { return module['default']; } :
+/******/ 			function getModuleExports() { return module; };
+/******/ 		__webpack_require__.d(getter, 'a', getter);
+/******/ 		return getter;
+/******/ 	};
+/******/
+/******/ 	// Object.prototype.hasOwnProperty.call
+/******/ 	__webpack_require__.o = function(object, property) { return Object.prototype.hasOwnProperty.call(object, property); };
+/******/
+/******/ 	// __webpack_public_path__
+/******/ 	__webpack_require__.p = "";
+/******/
+/******/ 	// Load entry module and return exports
+/******/ 	return __webpack_require__(__webpack_require__.s = 0);
+/******/ })
+/************************************************************************/
+/******/ ([
+/* 0 */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
 
-console.log('isAdult ', isAdult(18));
-console.log('canDrink', canDrink(18));
+"use strict";
+Object.defineProperty(__webpack_exports__, "__esModule", { value: true });
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_0__utils__ = __webpack_require__(1);
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_1__person__ = __webpack_require__(2);
+// import './utils';
+
+
+console.log('app.js is running');
+console.log('square ', __WEBPACK_IMPORTED_MODULE_0__utils__["b" /* square */](4));
+console.log('add', __WEBPACK_IMPORTED_MODULE_0__utils__["a" /* add */](2, 3));
+
+console.log('isAdult ', __WEBPACK_IMPORTED_MODULE_1__person__["b" /* isAdult */](18));
+console.log('canDrink', __WEBPACK_IMPORTED_MODULE_1__person__["a" /* canDrink */](18));
 
 
 // class IndecisionApp extends React.Component {
@@ -165,3 +238,35 @@ console.log('canDrink', canDrink(18));
 // }
 
 // ReactDOM.render(<IndecisionApp />, document.getElementById('app'));
+
+
+/***/ }),
+/* 1 */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+console.log('utils.js is running');
+
+const square = (x)=> x * x;
+/* harmony export (immutable) */ __webpack_exports__["b"] = square;
+ // inline export
+const add = (a, b)=> a + b;
+/* harmony export (immutable) */ __webpack_exports__["a"] = add;
+
+// export {square, add}; // named export
+
+
+/***/ }),
+/* 2 */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "b", function() { return isAdult; });
+/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "a", function() { return canDrink; });
+const isAdult = (age)=> age >= 18;
+const canDrink = (age)=> age >= 21;
+
+
+
+/***/ })
+/******/ ]);
--- src/playground/counter-example.js
+++ src/playground/counter-example.js
@@ -5,7 +5,19 @@ class Counter extends React.Component {
     this.handleMinusOne = this.handleMinusOne.bind(this);
     this.handleReset = this.handleReset.bind(this);
     this.state = {
-      count: props.count
+      count: 0
+    }
+  }
+  ComponentDidMount() {
+    const stringCount = localStorage.getItem('count');
+    const count = parseInt(stringCount, 10);
+    if (!isNaN(count)) {
+      this.setState(() => ({count}));
+    }
+  }
+  ComponentDidUpdate(prevProps, prevState) {
+    if (prevState.count !== this.state.count) {
+      localStorage.setItem('count', this.state.count);
     }
   }
   handleAddOne() {
@@ -44,9 +56,6 @@ class Counter extends React.Component {
       </div>
     );
   }
-}
-Counter.defaultProps = {
-  count: 0
 }
 
 ReactDOM.render(<Counter count={-10}/>, document.getElementById('app'));
